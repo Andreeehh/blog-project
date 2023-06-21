fc6f256229cfc0d5ddeefb1cd7e78cc6d286bba3
André Carvalho <andre.vrlcarvalho@hotmail.com>
2023-06-21 14:10:58 -0300

    ajustes no htmlcontent

--- src/components/HtmlContent/styles.ts
+++ src/components/HtmlContent/styles.ts
@@ -2,10 +2,11 @@ import styled, { css } from 'styled-components';
 
 export const Container = styled.footer`
   ${({ theme }) => css`
-    font-size: ${theme.font.sizes.medium};
+    font-size: calc(${theme.font.sizes.small} + 0.2rem);
+    line-height: 1.5;
 
     p {
-      margin: ${theme.spacings.xlarge} 0;
+      margin: ${theme.spacings.medium} 0;
     }
 
     a,
@@ -20,15 +21,29 @@ export const Container = styled.footer`
       filter: brightness(50%)
     }
 
+    code  {
+      font-family: monospace;
+      color: ${theme.colors.secondary};
+      font-size: ${theme.font.sizes.small};
+      background: ${theme.colors.mediumGray};
+      padding: 0.2rem;
+      margin: 0.2rem;
+    }
+
     pre {
       background: ${theme.colors.primary};
       padding: ${theme.spacings.medium};
       font-family: monospace;
       color: ${theme.colors.white};
-      margin: ${theme.spacings.xlarge} 0;
+      margin: ${theme.spacings.medium} 0;
       width: 100%;
       overflow-x: auto;
       font-size: ${theme.font.sizes.small};
+    }
+
+    pre code {
+      color: inherit;
+      background: inherit;
     }
 
     img {
@@ -38,7 +53,7 @@ export const Container = styled.footer`
     .image  {
       background: ${theme.colors.mediumGray};
       line-height: 0;
-      margin: ${theme.spacings.xlarge} 0;
+      margin: ${theme.spacings.medium} 0;
     }
 
     .image figcaption  {
@@ -60,13 +75,14 @@ export const Container = styled.footer`
 
     ul,
     ol  {
-      margin: ${theme.spacings.xlarge};
+      margin: ${theme.spacings.medium} ${theme.spacings.xlarge};
     }
 
     .table  {
       width: 100%;
       overflow: hidden;
       overflow-x: auto;
+      margin: ${theme.spacings.medium} 0;
     }
 
     table {
@@ -78,6 +94,15 @@ export const Container = styled.footer`
     table th  {
       padding: ${theme.spacings.small};
       border: 0.1rem solid ${theme.colors.mediumGray};
+    }
+
+    blockquote {
+      border-left: 0.5rem solid ${theme.colors.secondary};
+      color: ${theme.colors.darkerGray};
+      filter: brightness(80%);
+      padding-left: ${theme.spacings.medium};
+      font-style: italic;
+      margin: ${theme.spacings.medium};
     }
 
     @media ${theme.media.lteMedium} {
